3b0dea10ce8d05881ce2526d5fd38ac74bf27091
unknown <DAM121@iesataulfo.local>
2024-04-16 15:30:23 +0200

    Tercera Fase

--- src/directorioTrabajoAdanRomero/Clase.java
+++ src/directorioTrabajoAdanRomero/Clase.java
@@ -1,5 +1,9 @@
 package directorioTrabajoAdanRomero;
 
 public class Clase {
-
+	/**
+	 * Esta clase sera el objeto que despues crearemos y que,
+	 *  contendra el mensaje correspondiente.
+	 * 
+	 */
 }
