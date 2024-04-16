3cbbd02012ff273df507b5d5fa618458b8e25f21
unknown <DAM121@iesataulfo.local>
2024-04-16 15:24:07 +0200

    Segunda Fase

--- src/directorioTrabajoAdanRomero/Principal.java
+++ src/directorioTrabajoAdanRomero/Principal.java
@@ -1,5 +1,10 @@
 package directorioTrabajoAdanRomero;
 
 public class Principal {
-
+	/**
+	 * Esta es la clase Principal,
+	 *  esta clase solo contendra la creación del objeto Clase.
+	 *  
+	 */
+	
 }
